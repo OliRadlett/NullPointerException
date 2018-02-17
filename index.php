4583cad0d7fae7416bc545d7e5c5759123b37fed
Oli Radlett <o.radlett@gmail.com>
2018-02-17 16:50:45 +0000

    Update favicon

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "connect.php"; ?>
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 	<link rel="manifest" href="/site.webmanifest">
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-	<meta name="msapplication-TileColor" content="#2b5797">
+	<meta name="msapplication-TileColor" content="#9f00a7">
 	<meta name="theme-color" content="#ffffff">
     <title>NullPointerException</title>
 </head>
