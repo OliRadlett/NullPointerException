cdcbe3b09b65235842cfee394a057d7acbc5bb63
Oli Radlett <o.radlett@gmail.com>
2018-02-17 16:26:00 +0000

    Add favicon files and code

--- index.php
+++ index.php
@@ -6,8 +6,15 @@ include "connect.php"; ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <link rel="stylesheet" href="stylesheets/index.css" />
-      <script type="text/javascript" src = "scripts/logo-fix.js"></script>
+     <link rel="stylesheet" href="stylesheets/index.css" />
+     <script type="text/javascript" src = "scripts/logo-fix.js"></script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#2b5797">
+	<meta name="theme-color" content="#ffffff">
     <title>NullPointerException</title>
 </head>
 <body>
