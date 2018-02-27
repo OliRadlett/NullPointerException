0d67f07581090134c0649060b4224579b4d13f82
Oli Radlett <o.radlett@gmail.com>
2018-02-27 18:01:37 +0000

    Add header include to question.php and modify addActive.js to support showing the header on pages not included in the header

--- question.php
+++ question.php
@@ -19,37 +19,7 @@ include "questionFuncs.php" ?>
 
         }
     ?>
-    <img class = "nav-brand" src = "img/logo.png"/>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-        <ul class = "navbar-nav">
-            <li class = "nav-item">
-                <a class = "nav-link" href = "index.php">Home</a>
-            </li>
-            <?php
-                if (isset($_SESSION["username"])) {
-                    echo '<li class = "nav-item">';
-                    echo '<a class = "nav-link" href = "account.php">' . $_SESSION["username"] . '</a>';
-                    echo '</li>';
-                } else {
-                    echo '<li class = "nav-item">';
-                    echo '<a class = "nav-link" href = "signup.php">Sign Up/Login</a>';
-                    echo '</li>';
-                }
-            ?>
-            <li class = "nav-item">
-                <a class = "nav-link active" href = "qa.php">Q&A</a>
-            </li>
-            <li class = "nav-item">
-                <a class = "nav-link" href = "career.php">Careers</a>
-            </li>
-            <li class = "nav-item">
-                <a class = "nav-link" href = "tutorial.php">Tutorial zone</a>
-            </li>
-            <li class = "nav-item">
-                <a class = "nav-link" href = "comunity.php">Community</a>
-            </li>
-        </ul>
-    </nav>
+    <?php include ("header.html"); ?>
     <br/>
     <br/>
     <br/>
--- scripts/addActive.js
+++ scripts/addActive.js
@@ -2,12 +2,17 @@ window.onload = addActive;
 
 function addActive() {
 
+	let headerPages = ["index", "signup", "qa", "career", "tutorial", "community"]
+
 	let page = window.location.pathname;
 	page = page.replace("/", "");
 	page = page.replace(".php", "");
-	
-	document.getElementById(page).className += " active";
 
+	if (headerPages.indexOf(page) !== -1) {
+		
+		document.getElementById(page).className += " active";
+
+	}
 	// Overrides any window.onload event so run any JS functions in pages on header from here:
 
 	if (page == "qa") {
