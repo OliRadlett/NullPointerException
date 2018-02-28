8cda058d934d422c98c6ed45703c3aab14ca4b87
Oli Radlett <o.radlett@gmail.com>
2018-02-28 10:39:06 +0000

    Minor refactoring

--- processquestion.php
+++ processquestion.php
@@ -16,35 +16,31 @@ $query = "SELECT `address` FROM `blocked_ipaddr` WHERE `address` = '$address'";
 
 $result = mysqli_query($connection, $query);
     
-if ($result) {
-        
-    if (mysqli_num_rows($result) !== 1) {
+if (mysqli_num_rows($result) !== 1) {
     
-        if (!empty($title) && !empty($question)) {
+    if (!empty($title) && !empty($question)) {
 
-            $query = "INSERT INTO `questions` (`title`, `question`, `votes`, `time`) VALUES ('$title', '$question', '1', '$time')";
+        $query = "INSERT INTO `questions` (`title`, `question`, `votes`, `time`) VALUES ('$title', '$question', '1', '$time')";
 
-            if (!mysqli_query($connection, $query)) {
+        if (!mysqli_query($connection, $query)) {
                                 
-                echo mysqli_error($connection);
-                    
-            }
-
-            header("Location: nullpointerexception.ml/qa.php");
-
-        } else {
-
-            echo "Error - please make sure you have filled in every field";
-            echo "<br />";
-            echo "<button onclick='window.location.href = `http://www.nullpointerexception.ml/ask.php`';>Back</button>";
-
+            echo mysqli_error($connection);
+                
         }
+
+        header("Location: nullpointerexception.ml/qa.php");
 
     } else {
 
-        echo "Your IP Address has been blocked from asking questions";
+        echo "Error - please make sure you have filled in every field";
+        echo "<br />";
+        echo "<button onclick='window.location.href = `http://www.nullpointerexception.ml/ask.php`';>Back</button>";
 
     }
+
+} else {
+
+    echo "Your IP Address has been blocked from asking questions";
 
 }
 
