7c48ac7101ab134feb85bef1f97d32bd3fcc721b
Oli Radlett <o.radlett@gmail.com>
2018-03-24 15:13:13 +0000

    Add more content to variables.php

---
+++ sampleprograms/python3/variables.php
@@ -0,0 +1,48 @@
+<?php session_start();
+include "../../connect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel = "stylesheet" href = "../../stylesheets/index.css" />
+    <link rel = "stylesheet" href = "../../prism/prism.css" />
+    <script type="text/javascript" src = "../../prism/prism.js"></script>
+    <title>NullPointerException</title>
+</head>
+<body>
+    <?php include ("../../header.html"); ?>
+    <br/>
+    <br/>
+    <div class = "container">
+        <div class = "row">
+            <h1>Example of variables - Python3</h1>
+            <!--Aghhhh, all links and references break because its in a seperate directory to literaly everything-->
+        </div>
+    </div>
+    <br/>
+    <div class = "container">
+        <div class="row">
+            <div class = "col-lg-12">
+                <pre>
+                  <code class = "language-python">
+                    Integer = 12
+                    String = "Test string"
+                    Boolean = True
+                    Float = 7.9264
+                  </code>
+                </pre>
+            </div>
+        </div>
+        <div class = "row">
+          <div class = "col-lg-12">
+            <p>
+              This example shows how to declare variables of 4 common data types in Python3. In Python3, you don't need to specify the data type of a variable when you initialise it. This means that all variables are defined in the same way. Variables can also be initialised but not set to any value by setting it to equal None.
+            </p>
+            </p>
+          </div>
+        </div>
+    </div>
+</body>
+</html>
