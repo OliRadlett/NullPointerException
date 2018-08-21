a7dbb5357aab5638d0361d3d654bb805caae6e1a
Oli Radlett <o.radlett@gmail.com>
2018-08-21 18:20:03 +0100

    Minor refactor

--- processsignup.php
+++ processsignup.php
@@ -68,7 +68,8 @@ function UniqueUsername(Database $database, User $user) {
 	* 
 	*/
 
-	$database->query("SELECT `id` FROM npe.users WHERE `username` = '$user->getUsername()'");
+	$username = $user->getUsername();
+	$database->query("SELECT `id` FROM npe.users WHERE `username` = '$username'");
     $numRows = $database->numRows();
 
     if ($numRows == 0) {
@@ -84,41 +85,5 @@ function UniqueUsername(Database $database, User $user) {
     }
 
 }
-
-/*function CreateAccount($connection, User $user) {
-
-	/**
-	* Creates a new user record in the database
-	* 
-	* @param MySQLi connection $connection - Database connection object
-	* @param User $user - User object
-	* 
-	* @author Oli Radlett <o.radlett@gmail.com>
-	* 
-
-	// Prepares a query to insert all the User object's attributes into the database in their individual columns, skipping the auto-incrementing id column
-	$query = 
-	"INSERT INTO 
-	`users` (`username`, `password`, `first_name`, `last_name`, `email_address`)
-	VALUES
-	('$user->username', '$user->generateHash()', '$user->first_name', '$user->last_name', '$user->email_address');";
-    
-    // Executes the MySQLi query, creating a new user record in the database
-    mysqli_query($connection, $query);
-
-    // Prepares another query to get the auto-generated user id from the database
-    $query = "SELECT `id` FROM `users` WHERE `username` = '$username'";
-    // Executes the above query and stores the value of retrieved user id
-    $id = mysqli_query($connection, $query);
-
-    // Stores the username and user if in session variables, thus logging the user in
-    $_SESSION["username"] = $user->username;
-    $_SESSION["id"] = $id;
-
-    // Closes the session and re-directs the user to the homepage
-    session_write_close();
-    redirect("http://nullpointerexception.ml");
-
-}*/
 
 ?>
