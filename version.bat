40245664304b896b1ebc9802755450630a8083d7
Oli Radlett <o.radlett@gmail.com>
2018-09-20 19:51:26 +0100

    More writeup and deleted some old files

--- version.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-for /f "tokens=*" %%a in ('git rev-list --left-right --count origin/master...@') do set details=%%a
-echo %details%
-for /f "tokens=1,2,3,4 delims= " %%a in ("%details%") do set behind=%%a &set ahead=%%b
-echo %behind%
-echo %ahead%
