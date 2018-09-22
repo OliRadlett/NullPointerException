85f51e37f8ae0e6d6b49801dd92116d2eccd00c0
Oli Radlett <o.radlett@gmail.com>
2018-09-22 18:46:20 +0100

    Re-write admin page
    Initial re-write for admin page and processipblock

--- admin.php
+++ admin.php
@@ -1,21 +1,40 @@
-<?php session_start();
-include "connect.php";
+<!DOCTYPE html>
+<?php
 
+// Start PHP session
+session_start();
+
+// Include Database class
+include "database.php";
+
+// Create new Database object
+$database = new Database();
+
+// Is the current user logged in as admin
 if ($_SESSION["username"] == "admin") {
 
 ?>
-<form name = "blockip" method = "post" action = "processipblock.php">
-    <label>Block IP address</label>
-    <input type = "text" name = "ipaddr" />
-    <input type = "submit" value = "Block" />
-</form>
-<button onclick = "window.location.href='http://www.nullpointerexception.ml/'">Back</button>
+
+<head>
+    <title>Admin portal</title>
+</head>
+<body>
+    <!--Only render this form if the if statement passes-->
+    <form name = "blockip" method = "post" action = "processipblock.php">
+        <label>Block IP address</label>
+        <input type = "text" name = "ipaddr" />
+        <input type = "submit" value = "Block" />
+    </form>
+    <button onclick = "window.location.href='index.php'">Back</button>
+</body>
+
 <?php
 
 } else {
 
+//  Close headers and redirect user back to homepage
 	session_write_close();
-    header("Location: http://www.nullpointerexception.ml/");
+    header("Location: index.php");
 
 }
 ?>
