495187e6639471e0536f429f697cb3793a088087
Oli Radlett <o.radlett@gmail.com>
2018-09-27 13:50:19 +0100

    Initial re-write of question pages
    Add the initial re-write of question.php and questionFuncs.php to use the new Database class instead of the old connection.php

--- question.php
+++ question.php
@@ -1,6 +1,20 @@
-<?php session_start();
-include "connect.php";
-include "questionFuncs.php" ?>
+<?php
+
+/**
+ *
+ * Page to display a question to the user as well as show comments on the question
+ *
+ * @author Oli Radlett
+ *
+ */
+
+//  Start PHP session and include required files
+    session_start();
+    include "database.php";
+    include "questionFuncs.php"
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,14 +28,21 @@ include "questionFuncs.php" ?>
     <title>NullPointerException</title>
 </head>
 <body>
-    <?php include ("header.html");
-    $connection = connect()?>
+    <?php
+
+//      Include universal page header
+        include ("header.html");
+//      Create a new Database object
+        $database = new Database();
+
+    ?>
     <script type="text/javascript" src = "scripts/getUserVotes.js"></script>
     <br/>
     <br/>
     <br/>
     <?php
 
+//      Not sure what this function is for?
         function SetCookies() {
 
             if (isset($_GET["id"])) {
@@ -50,19 +71,20 @@ include "questionFuncs.php" ?>
 
         SetCookies();
 
-        $query = "SELECT * FROM `questions` WHERE `id` = '" . $_GET["id"] . "'";
-        $result = mysqli_query($connection, $query);
+//      Create and run a MySQL query to select all the attributes from the database for the question with the id attribute provided in the URL
+        $database->query("SELECT * FROM `questions` WHERE `id` = '" . $_GET["id"] . "'");
 
         // Declare multiple variables on the same line
         $qTitle = $qContent = $qVotes = $qTime = $qAuthor = "";
 
-        while($row = mysqli_fetch_assoc($result)) {
+//      Iterate over the attributes returned from the database and assign them to the variables created above
+        while($row = $database->fetchAssoc()) {
 
-            $qTitle =  $row["title"];
-            $qContent =  $row["question"];
-            $qVotes =  $row["votes"];
-            $qTime =  $row["time"];
-            $qAuthor =  $row["author"];
+            $qTitle = $row["title"];
+            $qContent = $row["question"];
+            $qVotes = $row["votes"];
+            $qTime = $row["time"];
+            $qAuthor = $row["author"];
 
         }
 
@@ -70,7 +92,13 @@ include "questionFuncs.php" ?>
     <div class = "container">
         <div class = "row">
             <div class = "col-lg-10">
-                <h2><?php echo $qTitle; ?></h2>
+                <h2>
+                    <?php
+
+                        echo $qTitle;
+
+                    ?>
+                </h2>
             </div>
         </div>
         <br/>
@@ -78,9 +106,14 @@ include "questionFuncs.php" ?>
             <div class = "col-lg-10">
                 <?php
 
+//                  Display the question, including adding syntax highlighting for code blocks
+
+//                  Create an array from the question content array by splitting it line by line
                     $questionArray = explode("\n", $qContent);
+//                  Call SplitLines function on newly created array
                     $splitArray = SplitLines($questionArray);
 
+//                  Iterate over each line of the array
                     for ($i = 0; $i < sizeof($questionArray); $i++) {
 
                         if (in_array($i, $splitArray[0])) {
@@ -146,7 +179,7 @@ include "questionFuncs.php" ?>
         <br/>
         <?php
 
-          GetComments($_GET["id"], $connection);
+          GetComments($_GET["id"], $database);
 
          ?>
     </div>
