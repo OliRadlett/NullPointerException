89d1fc1c55a56873a8378fb71bded051999a5349
Oli Radlett <o.radlett@gmail.com>
2018-09-27 19:05:50 +0100

    Initial re-write
    Add initial re-write of downloadsearchq.php to use the new Database class instead of the old connect.php

--- downloadsearchq.php
+++ downloadsearchq.php
@@ -1,13 +1,33 @@
 <?php
-include "connect.php";
-$connection = connect();
+
+/**
+ *
+ * Page to search, download and output the results of a search query
+ *
+ * @author Oli Radlett <o.radlett@gmail.com>
+ *
+ */
+
+// Include Database class
+include "database.php";
+
+// Create a new Database object
+$database = Database();
+
+// Get search query from URL
 $q = $_GET["q"];
-$query = "SELECT * FROM `questions` WHERE `title` LIKE '%{$q}%' OR `question` LIKE '%{$q}%'";
-$result = mysqli_query($connection, $query);
-while($row = mysqli_fetch_assoc($result)) {
+
+// Create an run a MySQL query to search the database based on the query stored above
+$database->query("SELECT * FROM `questions` WHERE `title` LIKE '%{$q}%' OR `question` LIKE '%{$q}%'");
+
+// Iterate over all the results from the database
+while($row = $database->fetchAssoc()) {
+
+//  Output the necessary attributes followed by a new-line
     echo $row["title"] . "<br/>";
     echo $row["id"] . "<br/>";
     echo $row["votes"] . "<br/>";
+
 }
-mysqli_close($connection);
+
 ?>
