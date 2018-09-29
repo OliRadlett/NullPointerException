b67c94c6dd3d29da86ac499967714b74f205e1d9
Oli Radlett <o.radlett@gmail.com>
2018-09-29 12:35:30 +0100

    Add initial re-write
    Add initial re-write for modify vote.php to use the new Database class instead of the old connect.php method. Also add comments.

--- modifyvote.php
+++ modifyvote.php
@@ -1,59 +1,167 @@
 <?php
-include "connect.php";
-$connection = connect();
-$qID = $_GET["qid"];
-$username = $_GET["username"];
-$func = $_GET["func"];
-$uID = getUserID($connection, $username);
-// Oops, this isn't very secure...
-// Might be an idea to fix this
-// *At some point*
-// Blah blah can run preset SQL querys from by using URL params blah blah
-switch ($func) {
-    case 'removevote':
-        RemoveVote($connection, $qID, $uID);
-        break;
-    case 'addupvote':
-        AddUpVote($connection, $qID, $uID);
-        break;
-    case 'adddownvote':
-        AddDownVote($connection, $qID, $uID);
-        break;
-    default:
-        break;
-}
-function AlreadyVoted($connection, $qID, $uID) {
-    $query = "SELECT `id` FROM `votes` WHERE (`qID` = '$qID' AND `uID` = '$uID')";
-    $result = mysqli_query($connection, $query);
-    if (isset($result) && mysqli_num_rows($result) > 0) {
-        return true;
-    } else {
-        return false;
+
+//  Include Database class
+    include "database.php";
+
+//  Create a new Database object
+    $database = new Database();
+
+//  Create and initialise variables from URL parameters
+    $qID = $_GET["qid"];
+    $username = $_GET["username"];
+    $func = $_GET["func"];
+
+
+//  Get the user id of the user with the username provided in the URL
+    $uID = getUserID($database, $username);
+
+    // Can run preset SQL queries from by using URL params
+    // This is insecure, fix this
+
+//  Switch over the $func variable and run the requisite function
+//  Do nothing if $func does not equal any of the cases
+    switch ($func) {
+
+        case 'removevote':
+            RemoveVote($database, $qID, $uID);
+            break;
+
+        case 'addupvote':
+            AddUpVote($database, $qID, $uID);
+            break;
+
+        case 'adddownvote':
+            AddDownVote($database, $qID, $uID);
+            break;
+
+        default:
+            break;
+
     }
 
-}
-function AddUpVote($connection, $qID, $uID) {
-    if (!AlreadyVoted($connection, $qID, $uID)) {
-        $query = "INSERT INTO `votes` (`qID`, `uID`, `type`) VALUES ('$qID', '$uID', 'u')";
-        mysqli_query($connection, $query);
-        mysqli_close($connection);
+    function AlreadyVoted(Database $database, $qID, $uID) {
+
+        /**
+         *
+         * Function to check if a user has already voted on a question
+         *
+         * @param Database: $database - Database object
+         * @param Int: $qID - The id of the question to check if the user has voted on
+         * @param Int: $uID - The id of the user to check if they have voted on a question
+         *
+         * @return Boolean: true if the user with the user id provided in $uID has voted on the
+         * question with the id provided in $qID. false if the user hasn't
+         *
+         * @author Oli Radlett <o.radlett@gmail.com>
+         *
+         */
+
+//      Prepare and run a MySQL query to select the id attribute from the votes table if the qID attribute
+//      matches the $qID parameter and the uID attribute matches the $uID parameter
+        $database->query("SELECT `id` FROM `votes` WHERE (`qID` = '$qID' AND `uID` = '$uID')");
+
+//      If there is a result from the query then the user must have voted on the question
+        if (isset($result) && $database->numRows() > 0) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
     }
-}
-function AddDownVote($connection, $qID, $uID) {
-    if (!AlreadyVoted($connection, $qID, $uID)) {
-        $query = "INSERT INTO `votes` (`qID`, `uID`, `type`) VALUES ('$qID', '$uID', 'd')";
-        mysqli_query($connection, $query);
-        mysqli_close($connection);
+    function AddUpVote(Database $database, $qID, $uID) {
+
+        /**
+         *
+         * Function to insert a positive vote into the database
+         *
+         * @param Database: $database - Database object
+         * @param Int: $qID - The id of the question to add the positive vote to
+         * @param Int: $uID - The id of the user that added the positive vote
+         *
+         * @author Oli Radlett <o.radlett@gmail.com>
+         *
+         */
+
+//      Check that the user hasn't already voted
+        if (!AlreadyVoted($database, $qID, $uID)) {
+
+//          Prepare and run a MySQL query to insert a positive vote into the database
+            $database->query("INSERT INTO `votes` (`qID`, `uID`, `type`) VALUES ('$qID', '$uID', 'u')");
+
+        }
+
     }
-}
-function RemoveVote($connection, $qID, $uID) {
-    $query = "DELETE FROM `votes` WHERE (`votes`.`qID` = '$qID' AND `votes`.`uID` = '$uID')";
-    mysqli_query($connection, $query);
-    mysqli_close($connection);
-}
-function getUserID($connection, $username) {
-    $query = "SELECT `id` FROM `users` WHERE `username` = '$username'";
-    $result = mysqli_fetch_assoc(mysqli_query($connection, $query));
-    return $result["id"];
-}
+
+    function AddDownVote(Database $database, $qID, $uID) {
+
+        /**
+         *
+         * Function to insert a negative vote into the database
+         *
+         * @param Database: $database - Database object
+         * @param Int: $qID - The id of the question to add the negative vote to
+         * @param Int: $uID - The id of the user that added the negative vote
+         *
+         * @author Oli Radlett <o.radlett@gmail.com>
+         *
+         */
+
+//      Check that the user hasn't already voted
+        if (!AlreadyVoted($database, $qID, $uID)) {
+
+//          Prepare and run a MySQL query to insert a negative vote into the database
+            $database->query("INSERT INTO `votes` (`qID`, `uID`, `type`) VALUES ('$qID', '$uID', 'd')");
+
+        }
+
+    }
+
+    function RemoveVote(Database $database, $qID, $uID) {
+
+        /**
+         *
+         * Function to remove a vote from the database
+         *
+         * @param Database: $database - Database object
+         * @param Int: $qID - The id of the question to remove the vote from
+         * @param Int: $uID - The id of the user who's vote needs to be removed
+         *
+         * @author Oli Radlett <o.radlett@gmail.com>
+         *
+         */
+//
+//      Prepare and run a MySQL query to remove a vote record from the database
+        $database->query("DELETE FROM `votes` WHERE (`votes`.`qID` = '$qID' AND `votes`.`uID` = '$uID')");
+
+    }
+
+    function getUserID(Database $database, $username) {
+
+        /**
+         *
+         * Function to remove to get the user id attribute for a user
+         *
+         * @param Database: $database - Database object
+         * @param String: $username - Username of the user who's id we want
+         *
+         * @return Int: The id of the user if the username exists. Null if the user does not exist
+         *
+         * @author Oli Radlett <o.radlett@gmail.com>
+         *
+         */
+
+//      Prepare and run a MySQL query to select the id attribute from the users table where the username
+//      attribute is equal to the $username parameter
+        $database->query("SELECT `id` FROM `users` WHERE `username` = '$username'");
+//      Create an associative array of the result of the query
+        $result = $database->fetchAssoc();
+//      Return the user's id
+        return $result["id"];
+
+    }
+
 ?>
