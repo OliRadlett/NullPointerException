1eb12cd92b7115aa807bcec05353004bcdd93765
Oli Radlett <o.radlett@gmail.com>
2018-09-30 22:34:03 +0100

    Minor refactor and add comments
    Change var to let and add comments for logo-fix.js

--- scripts/logo-fix.js
+++ scripts/logo-fix.js
@@ -1,20 +1,34 @@
+/**
+ *
+ * Script used to change the logo to a shorter version if the browser width goes below a certain value
+ *
+ * @author Oli Radlett <o.radlett@gmail.com>
+ *
+ */
+
+// Run the checkLogo() function when the page first loads and every time the browser window changes size
 window.onload = checkLogo;
 window.onresize = checkLogo;
 
 
 function checkLogo() {
 
-	var width = window.innerWidth;
-	var logo = document.getElementById("logo");
-	var fullLogo = "img/logo.png";
-	var mobileLogo = "img/logo-MOBILE.png";
+    // Get the current width of the browser
+	let width = window.innerWidth;
+	// Get a reference to the logo element
+	let logo = document.getElementById("logo");
+	// Create Strings for the paths of the two logos
+	let fullLogo = "img/logo.png";
+	let mobileLogo = "img/logo-MOBILE.png";
 
 	if (width > 576) {
 
+	    // Apply full size logo if browser is wide enough
 		logo.src = fullLogo;
 
 	} else {
 
+	    // Apply shorter logo if browser is too narrow
 		logo.src = mobileLogo;
 
 	}
